420391f6a6b548cfbebce0b427125f66b099601e
algoroloro <algoro9697@gmail.com>
2020-12-29 22:17:00 +0100

    crud service completado

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import {CrudService} from './modules/shared/services/crud.service';
-import {PREFIX} from './modules/shared/config/webApiPrefix.config';
 
 @Component({
   selector: 'ape-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-
-  constructor(private crud: CrudService) {
-    this.crud.withUrlPrefix(PREFIX.catalogo);
-  }
-}
+export class AppComponent {}
--- frontend/src/app/modules/shared/services/crud.service.ts
+++ frontend/src/app/modules/shared/services/crud.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
 import { BASEURL } from 'src/app/modules/shared/config/webApiPrefix.config';
 
 @Injectable({
@@ -7,10 +11,30 @@ import { BASEURL } from 'src/app/modules/shared/config/webApiPrefix.config';
 export class CrudService {
   apiRest: string;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   withUrlPrefix(urlPrefix: string): void {
     this.setUrlPrefix(urlPrefix);
+  }
+
+  create(item: object): Observable<object> {
+    return this.http.post(`${this.apiRest}/create`, item);
+  }
+
+  get(): Observable<object> {
+    return this.http.get(`${this.apiRest}/read`);
+  }
+
+  getById(itemId: string): Observable<object> {
+    return this.http.get(`${this.apiRest}/read/${itemId}`);
+  }
+
+  updateById(itemId: string, item: object): Observable<object> {
+    return this.http.put(`${this.apiRest}/update/${itemId}`, item);
+  }
+
+  deleteById(itemId: string): Observable<object> {
+    return this.http.delete(`${this.apiRest}/delete/${itemId}`);
   }
 
   private setUrlPrefix(urlPrefix: string): void {
