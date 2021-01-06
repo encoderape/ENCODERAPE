0734962a8cf93cb46b2fb4a00442e7353b582ee7
algoroloro <algoro9697@gmail.com>
2021-01-06 09:53:40 +0100

    title service

---
+++ frontend/src/app/modules/shared/services/title.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TitleService {
+  constructor(
+    private title: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  initializeTitleService(): void {
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const { data } = this.activatedRoute.root.firstChild.snapshot;
+        const mainTitle = 'ENCODERAPE';
+        const lastTitle = this.title.getTitle();
+        if (data?.title) {
+          const title = `${data.title} | ${mainTitle}`;
+          if (lastTitle !== title) {
+            this.title.setTitle(title);
+          }
+        } else {
+          if (lastTitle !== mainTitle) {
+            this.title.setTitle(mainTitle);
+          }
+        }
+      });
+  }
+}
