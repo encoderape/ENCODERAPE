59fcce07ac397fe045135bc643e097aac9d31fe5
Ramses Moreno Rodriguez <ramsesmorenorodriguez@Ramses.local>
2021-01-07 19:52:55 +0100

    modelo nosotros terminado

---
+++ frontend/src/app/modules/nosotros/models/cursos.ts
@@ -0,0 +1,11 @@
+export class Cursos {
+    fechaFin: Date;
+    nombre: string;
+    aprendizaje: string;
+
+    constructor(fechaFin, nombre, aprendizaje) {
+        this.fechaFin = fechaFin;
+        this.nombre = nombre;
+        this.aprendizaje = aprendizaje;
+    }
+}
---
+++ frontend/src/app/modules/nosotros/models/tecnologias.ts
@@ -0,0 +1,11 @@
+export class Tecnologias {
+  imagen: string;
+  nombre: string;
+  progreso: number;
+
+  constructor(imagen, nombre, progreso) {
+    this.imagen = imagen;
+    this.nombre = nombre;
+    this.progreso = progreso;
+  }
+}
