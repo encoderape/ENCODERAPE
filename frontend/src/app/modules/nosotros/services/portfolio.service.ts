416137605264cc97db20267a7ad5a75951c1a697
Ramses797 <rmorenor97@gmail.com>
2021-01-13 21:29:48 +0100

    servicio del portfolio creado con la herencia

--- frontend/src/app/modules/nosotros/services/portfolio.service.ts
+++ frontend/src/app/modules/nosotros/services/portfolio.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { CrudService } from 'src/app/modules/shared/services/crud.service';
 import { PREFIX } from 'src/app/modules/shared/config/webApiPrefix.config';
+import { Portfolio } from 'src/app/modules/nosotros/models/portfolio/portfolio';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PortfolioService {
-  constructor(private crud: CrudService) {
-    this.crud.withUrlPrefix(PREFIX.portfolio);
+export class PortfolioService extends CrudService<Portfolio>{
+  constructor(http: HttpClient) {
+    super(http);
+    this.withCasting(x => new Portfolio(x)).withUrlPrefix(PREFIX.portfolio);
   }
 
-  get(): Observable<object> {
-    return this.crud.get();
-  }
 }
