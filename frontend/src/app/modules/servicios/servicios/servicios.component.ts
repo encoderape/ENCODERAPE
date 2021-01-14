bbf103a25c7c013eba02f1082549fff5039c3078
Ramses797 <rmorenor97@gmail.com>
2021-01-14 21:45:22 +0100

    servicio.component hecho

--- frontend/src/app/modules/servicios/servicios/servicios.component.ts
+++ frontend/src/app/modules/servicios/servicios/servicios.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ServiciosService } from 'src/app/modules/servicios/services/servicios.sevice';
+
 @Component({
   selector: 'ape-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.scss'],
 })
 export class ServiciosComponent implements OnInit {
-  constructor() {}
+  constructor(private serviciosService: ServiciosService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.leerServicios();
+  }
+
+  private leerServicios(): void {
+    console.log(this.serviciosService.read().subscribe());
+  }
 }
