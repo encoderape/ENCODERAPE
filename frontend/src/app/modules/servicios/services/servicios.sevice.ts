bd2b13dd315cd373eee1c8ff087fc6f91d253079
Ramses797 <rmorenor97@gmail.com>
2021-01-14 21:33:19 +0100

    servicio de servicios hecho

--- frontend/src/app/modules/servicios/services/servicios.sevice.ts
+++ frontend/src/app/modules/servicios/services/servicios.sevice.ts
@@ -4,3 +4,18 @@ import { Observable } from 'rxjs';
 
 import { CrudService } from 'src/app/modules/shared/services/crud.service';
 import { PREFIX } from 'src/app/modules/shared/config/webApiPrefix.config';
+import { Servicios } from 'src/app/modules/servicios/models/servicios';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class ServiciosService extends CrudService<Servicios> {
+    constructor(http: HttpClient) {
+      super(http);
+      this.withUrlPrefix(PREFIX.servicios);
+    }
+
+    read(): Observable<Servicios[]> {
+      return this.get();
+    }
+  }
