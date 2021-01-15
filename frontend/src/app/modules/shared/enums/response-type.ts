6c9d0a1e73b74d4dfe09c3ecd842e2cc982049fb
algoroloro <algoro9697@gmail.com>
2021-01-15 17:03:02 +0100

    Realizado Warning Interceptor

--- frontend/src/app/modules/shared/enums/response-type.ts
+++ frontend/src/app/modules/shared/enums/response-type.ts
@@ -2,6 +2,7 @@ export enum ResponseType {
   OK = 200,
   CREATED = 201,
   BAD_REQUEST = 400,
+  UNAUTOHORIZED = 401,
   NOT_FOUND = 404,
   SERVER_ERROR = 500,
 }
--- frontend/src/app/modules/shared/interceptors/request-warning.interceptor.ts
+++ frontend/src/app/modules/shared/interceptors/request-warning.interceptor.ts
@@ -1,20 +1,49 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
+  HttpErrorResponse,
   HttpEvent,
+  HttpHandler,
   HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ResponseType } from 'src/app/modules/shared/enums/response-type';
+import { ToastService } from 'src/app/modules/shared/services/toast.service';
+import { LoggerService } from 'src/app/modules/shared/services/logger.service';
+import { Log } from 'src/app/modules/shared/models/log';
 
 @Injectable()
 export class RequestWarningInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(
+    private toastService: ToastService,
+    private router: Router,
+    private loggerService: LoggerService
+  ) {}
 
   intercept(
-    request: HttpRequest<unknown>,
+    request: HttpRequest<any>,
     next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  ): Observable<HttpEvent<any>> {
+    const log: Log = {
+      code: 0,
+      error: '',
+    };
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === ResponseType.UNAUTOHORIZED) {
+          log.code = 401;
+          log.error = 'No tienes acceso a este contenido.';
+          this.loggerService.insert(log);
+          this.loggerService.console(log);
+          this.toastService.callWarningToast(
+            'Está intentando acceder a una URL a la que solo los administradores pueden acceder.'
+          );
+          this.router.navigate(['/']);
+          return throwError(err);
+        }
+      })
+    );
   }
 }
