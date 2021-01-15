0cc469454442c6682610f925248c4e6d9a97877c
algoroloro <algoro9697@gmail.com>
2021-01-15 17:14:20 +0100

    Backend del Log

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ const CORS = require('./middlewares/cors.js');
 // ROUTER IMPORTS
 const CATALOGO = require('./routes/_catalogo.js');
 const CONTACTO = require('./routes/_contacto.js');
+const LOG = require('./routes/_log.js');
 const NOSOTROS = require('./routes/_nosotros.js');
 const PORTFOLIO = require('./routes/_portfolio.js');
 const SERVICIOS = require('./routes/_servicios.js');
@@ -46,6 +47,7 @@ APP.use(CORS);
 // ENDPOINTS
 APP.use('/catalogo', CATALOGO);
 APP.use('/contacto', CONTACTO);
+APP.use('/log', LOG);
 APP.use('/nosotros', NOSOTROS);
 APP.use('/portfolio', PORTFOLIO);
 APP.use('/servicios', SERVICIOS);
