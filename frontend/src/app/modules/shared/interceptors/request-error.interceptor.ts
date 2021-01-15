c4aea2653ec88108e26bc4609aa6726dda6f64a8
algoroloro <algoro9697@gmail.com>
2021-01-15 17:02:32 +0100

    Insertando Logger Service en los Interceptors de Error y Success

--- frontend/src/app/modules/shared/interceptors/request-error.interceptor.ts
+++ frontend/src/app/modules/shared/interceptors/request-error.interceptor.ts
@@ -10,32 +10,54 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ResponseType } from 'src/app/modules/shared/enums/response-type';
 import { ToastService } from 'src/app/modules/shared/services/toast.service';
+import { LoggerService } from 'src/app/modules/shared/services/logger.service';
+import { Log } from 'src/app/modules/shared/models/log';
 
 @Injectable()
 export class RequestErrorInterceptor implements HttpInterceptor {
-  constructor(private toastService: ToastService) {}
+  constructor(
+    private toastService: ToastService,
+    private loggerService: LoggerService
+  ) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const log: Log = {
+      code: 0,
+      error: '',
+    };
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
         if (err.status === ResponseType.BAD_REQUEST) {
+          log.code = 400;
+          log.error =
+            'La URL a la que se ha intentado hacer la petición está mal.';
+          this.loggerService.insert(log);
+          this.loggerService.console(log);
           this.toastService.callErrorToast(
-            'Petición equivocada, ponte en contacto con nosotros si el problema persiste'
+            'Petición equivocada, ponte en contacto con nosotros si el problema persiste.'
           );
           return throwError(err);
         }
         if (err.status === ResponseType.NOT_FOUND) {
+          log.code = 404;
+          log.error = 'Se ha intentado buscar algo que no existe.';
+          this.loggerService.insert(log);
+          this.loggerService.console(log);
           this.toastService.callErrorToast(
-            'No se ha podido encontrar lo que buscas, ponte en contacto con nosotros si el problema persiste'
+            'No se ha podido encontrar lo que buscas, ponte en contacto con nosotros si el problema persiste.'
           );
           return throwError(err);
         }
         if (err.status === ResponseType.SERVER_ERROR) {
+          log.code = 500;
+          log.error = 'Ha habido un error interno del servidor.';
+          this.loggerService.insert(log);
+          this.loggerService.console(log);
           this.toastService.callErrorToast(
-            'Error interno del servidor, ponte en contacto con nosotros si el problema persiste'
+            'Error interno del servidor, ponte en contacto con nosotros si el problema persiste.'
           );
           return throwError(err);
         }
--- frontend/src/app/modules/shared/interceptors/request-success.interceptor.ts
+++ frontend/src/app/modules/shared/interceptors/request-success.interceptor.ts
@@ -9,24 +9,39 @@ import {
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-
 import { ResponseType } from 'src/app/modules/shared/enums/response-type';
 import { ToastService } from 'src/app/modules/shared/services/toast.service';
+import { LoggerService } from 'src/app/modules/shared/services/logger.service';
+import { Log } from 'src/app/modules/shared/models/log';
 
 @Injectable()
 export class RequestSuccessInterceptor implements HttpInterceptor {
-  constructor(private toastService: ToastService, private router: Router) {}
+  constructor(
+    private toastService: ToastService,
+    private router: Router,
+    private loggerService: LoggerService
+  ) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const log: Log = {
+      code: 0,
+      error: '',
+    };
     return next.handle(request).pipe(
       tap((event: HttpHeaderResponse) => {
         if (event.status === ResponseType.OK) {
+          log.code = 200;
+          log.error = 'Datos obtenidos correctamente.';
+          this.loggerService.console(log);
           return;
         }
         if (event.status === ResponseType.CREATED) {
+          log.code = 201;
+          log.error = 'Datos insertados correctamente.';
+          this.loggerService.console(log);
           this.toastService.callSuccessfulToast('Formulario enviado con éxito');
           this.router.navigate(['/']);
         }
