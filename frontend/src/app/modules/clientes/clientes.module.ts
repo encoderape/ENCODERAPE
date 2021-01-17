bb3af45cefe4de5a0d495c162fe0a71c616072e7
Ramses797 <rmorenor97@gmail.com>
2021-01-17 19:31:08 +0100

    mixins responsive creados

--- frontend/src/app/modules/clientes/clientes.module.ts
+++ frontend/src/app/modules/clientes/clientes.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientesRouting } from 'src/app/modules/clientes/clientes.routing';
-import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
+// import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
 
 @NgModule({
-  declarations: [NuestrosClientesComponent],
+  declarations: [/*NuestrosClientesComponent*/],
   imports: [CommonModule, ClientesRouting],
 })
 export class ClientesModule {}
--- frontend/src/app/modules/clientes/clientes.routing.ts
+++ frontend/src/app/modules/clientes/clientes.routing.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
+// import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
 
-const routes: Routes = [{ path: '', component: NuestrosClientesComponent }];
+// const routes: Routes = [{ path: '', component: NuestrosClientesComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    /*RouterModule.forChild(routes)*/
+  ],
   exports: [RouterModule],
 })
 export class ClientesRouting {}
