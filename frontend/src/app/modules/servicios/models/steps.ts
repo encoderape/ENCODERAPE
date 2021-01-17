5448dffe9b086011c3f29cc827d497e967345d6d
Ramses797 <rmorenor97@gmail.com>
2021-01-17 17:34:04 +0100

    cambio en modelo sevicios

--- frontend/src/app/modules/servicios/models/steps.ts
+++ frontend/src/app/modules/servicios/models/steps.ts
@@ -1,7 +1,6 @@
 import { Caracteristicas } from 'src/app/modules/servicios/models/caracteristicas';
 
 export type Steps = {
-  imagen: string;
   titulo: string;
   caracteristicas: Caracteristicas[];
 };
