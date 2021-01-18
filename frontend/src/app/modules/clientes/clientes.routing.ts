dcffdfb20380f21695010f82f55142910f4b8c90
algoroloro <algoroloro@gmail.com>
2021-01-18 09:32:06 +0100

    Clientes y Comentarios completado

--- frontend/src/app/modules/clientes/clientes.routing.ts
+++ frontend/src/app/modules/clientes/clientes.routing.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
+import { NuestrosClientesComponent } from 'src/app/modules/clientes/nuestros-clientes/nuestros-clientes.component';
 
-// const routes: Routes = [{ path: '', component: NuestrosClientesComponent }];
+const routes: Routes = [{ path: '', component: NuestrosClientesComponent }];
 
 @NgModule({
-  imports: [
-    /*RouterModule.forChild(routes)*/
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class ClientesRouting {}
